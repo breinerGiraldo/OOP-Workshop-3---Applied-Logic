0e83c2e6ad2776193c0b9ae723e573192388f0ec
Retoto <brecas1820@gmail.com>
2026-03-06 19:20:18 -0500

    exercise 35

--- exercice_30/WorkShop_35/Program.cs
+++ exercice_30/WorkShop_35/Program.cs
@@ -3,8 +3,19 @@
 
 using Shared;
 
+var answer = string.Empty;
+var options = new List<string> { "s", "n" };
 
-var num = ConsoleExtension.GetInt("Ingrese el número a descomponer:");
+do
+{
+    var num = ConsoleExtension.GetInt("Ingrese el número a descomponer:");
 
         FactorOperations.DecomposeNumber(num);
- 
+    do
+    {
+        answer = ConsoleExtension.GetValidOptions("Desea continuar [S]i , [N]o?: ", options);
+    } while (!options.Any(x => x.Equals(answer, StringComparison.CurrentCultureIgnoreCase)));// validamos que la respuesta del usuario sea igual a alguna de las opciones que tenemos en la lista, el string comparison es para validar que no importe si el usuario escribe mayuscula o minuscula 
+
+} while (answer!.Equals("s", StringComparison.CurrentCultureIgnoreCase));
+Console.WriteLine("F mi loco ");
+
