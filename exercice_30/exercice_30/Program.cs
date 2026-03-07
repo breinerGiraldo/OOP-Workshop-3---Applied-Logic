a4d4a736a630c10dd8ea6f9d50d953681471a76e
Retoto <brecas1820@gmail.com>
2026-03-06 19:22:46 -0500

    exercice 35

--- exercice_30/exercice_30/Program.cs
+++ exercice_30/exercice_30/Program.cs
@@ -22,7 +22,7 @@ Console.WriteLine("Mayor elemento: " + elderly);
 do
 {
     answer = ConsoleExtension.GetValidOptions("Desea continuar [S]i , [N]o?: ", options);
-} while (!options.Any(x => x.Equals(answer, StringComparison.CurrentCultureIgnoreCase)));// validamos que la respuesta del usuario sea igual a alguna de las opciones que tenemos en la lista, el string comparison es para validar que no importe si el usuario escribe mayuscula o minuscula 
+} while (!options.Any(x => x.Equals(answer, StringComparison.CurrentCultureIgnoreCase)));
 
 } while (answer!.Equals("s", StringComparison.CurrentCultureIgnoreCase)) ;
 Console.WriteLine("F mi loco ");
